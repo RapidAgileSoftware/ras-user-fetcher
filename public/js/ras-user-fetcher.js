234c945c355433a80772e3baac61774846eabb35
Jens Krause <jens@rasta.online>
2020-03-17 16:24:56 +0700

    add childTable for additional User Info

--- public/js/ras-user-fetcher.js
+++ public/js/ras-user-fetcher.js
@@ -1,10 +1,36 @@
 (function($) {
 	'use strict';
-	//console.log('Hey-ya');
-	console.log(php_vars);
 
+	function showUserDetails(user){
+		
+		// initiate a dedicated child table
+        $('#ras-user-fetcher').jtable('openChildTable', $('#ras-user-fetcher-details'),
+            {
+                title: user.name + ' - Details',
+                	actions: {
+                    	listAction: php_vars.user_endpoint +'&user_id='+ user.id,
+                    },
+                    fields: {
+                    	id: {
+                        	title: 'Id',
+							key: true,
+							list: true,
+							width: '20%',
+                            },
+                        name: {
+							title: 'Name',
+							width: '40%',	
+							},          
+                    }
+            }, 
+            function (data) { //opened handler
+            	data.childTable.jtable('load');
+            });
+                     
+	}
 
     $(document).ready(function() {
+    	// Initiate parent User table
     	$('#ras-user-fetcher').jtable({
 			title: 'Here are some awesome users ...',
 			actions: {
@@ -15,21 +41,37 @@
 						title: 'Id',
 						key: true,
 						list: true,
-						width: '20%'
+						width: '20%',
+						display: function (user){
+							var $link = $('<a href="#'+user.record.id+'">'+user.record.id+"</a>");
+							$link.click(function () { showUserDetails(user.record); });
+							return $link;
+							}
 
 					},
 				name: {
 						title: 'Name',
-						width: '40%'
+						width: '40%',
+						display: function (user){
+							var $link = $('<a href="#'+user.record.id+'">'+user.record.name+"</a>");
+							$link.click(function () { showUserDetails(user.record); });
+							return $link;
+							}
 					},
 				username:{
 						title: 'User Name',
-						width: '40%'
-				}	
-			}	
+						width: '40%',
+						display: function (user){
+							var $link = $('<a href="#'+user.record.id+'">'+user.record.username+"</a>");
+							$link.click(function () { showUserDetails(user.record); });
+							return $link;
+							}
+						}
+					}	
 			});
 
 		$('#ras-user-fetcher').jtable('load');
+		
        
 
     });
